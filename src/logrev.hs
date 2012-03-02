71738c9cb08e9d258862434211f52a2eca45b82b
Daniel Molina Wegener <dmw@coder.cl>
2012-03-02 12:51:16 -0300

    Adde logrev.hs header.

--- src/logrev.hs
+++ src/logrev.hs
@@ -1,3 +1,17 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Daniel Molina Wegener 2012
+-- License     :  BSD 3 (see the LICENSE file)
+-- Author      :  Daniel Molina Wegener <dmw@coder.cl>
+-- Homepage    :  http://coder.cl/products/logrev/
+-- Repository  :  https://github.com/dmw/ApacheLogRev
+--
+-- An Apache Access Log Statistics extractor
+--
+-- This is the initial commit of this project, please write
+-- me directly if you want to contribute.
+-----------------------------------------------------------------------------
 
 module Main (main) where
 
